e7bd13e30bf0e7e12f6c020d35e402bbdbc6ec0c
qzl1994 <305165598@qq.com>
2016-10-18 14:40:58 +0800

    控制飞机移动，限定边界

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    [System.Serializable]
+    public class Boundary
+    {
+        public float xMax;
+        public float xMin;
+        public float zMax;
+        public float zMin;
+    }
+
+    public float Speed;
+    public Boundary boundray;
+
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+        GetComponent<Rigidbody>().velocity = movement * Speed;
+        GetComponent<Rigidbody>().position = new Vector3(
+            Mathf.Clamp(GetComponent<Rigidbody>().position.x,boundray.xMin,boundray.xMax),
+            0.0f,
+            Mathf.Clamp(GetComponent<Rigidbody>().position.z,boundray.zMin,boundray.zMax)
+            );
+    }
+}
