e8e037aded9e0042391e33ddb112e8a19150bd54
qzl1994 <305165598@qq.com>
2016-10-18 17:52:35 +0800

    创建敌机和子弹

---
+++ Assets/Scripts/WeaponController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class WeaponController : MonoBehaviour {
+
+    public GameObject Shot;
+    public Transform ShotSpawn;
+    public float Delay;
+    public float FireRate;
+
+	void Start ()
+    {
+        InvokeRepeating("Fire", Delay, FireRate);
+	}
+    
+    void Fire()
+    {
+        Instantiate(Shot, ShotSpawn.position, ShotSpawn.rotation);
+        GetComponent<AudioSource>().Play();
+    }
+}
